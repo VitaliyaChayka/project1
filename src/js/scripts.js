0e34b00fb86ec39ee6d87a369455654da122943b
Vitaliya_Chayka <vialife79@gmail.com>
2021-09-08 23:52:49 +0300

    first commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,87 +1,34 @@
-///////////burger///////
-
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.nav__list');
-const closeBtn = document.querySelector('.close-btn');
-const links = document.querySelectorAll('nav ul li a');
-
-burger.addEventListener('click', e => menu.classList.toggle('open'));
-closeBtn.addEventListener('click', e => menu.classList.remove('open'));
-[...links].forEach(link => link.addEventListener('click', e => menu.classList.remove('open')));
-
-//////////////////////accordion////////
-
-let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-   let panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-
-////////////////////////animation///
-var button = document.querySelector(".form-btn__main");
-
-button.onmousemove = (e) => {
-   var x = e.pageX - e.target.offsetLeft;
-   var y = e.pageY - e.target.offsetTop;
-
-   button.style.setProperty("--x", x + "px");
-   button.style.setProperty("--y", y + "px");
-};
-
-
 ////////////////////////slider////
 
-$('.home-slider').slick({
-  dots: false,
-  arrows: false,
+$('.slider').slick({
+  arrows:true,
+  dots:true,
   autoplay: true,
   infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+  adaptiveHeight: true,
+  easing: 'linear',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed:1000,
+  pauseOnFocus:true,
+  responsive: [
+    {
+      breakpoint: 580,
+      settings: {
+        arrows: false,
+      
+      }
+    }
+  ]
 });
-////////////////////////modal
 
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-/////////////////////modal2
-let modal2 = document.getElementById("Modal2");
-let btn2 = document.getElementById("Link");
-let span2 = document.getElementsByClassName("close2")[0];
-btn2.onclick = function() {
-  modal2.style.display = "block";
-}
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
+
 
 ////////////////////////////////// icon scrollup
+let wHeight = window.innerHeight;
+window.addEventListener('resize', function(){
+    wHeight = window.innerHeight;
+})
 
 let upBtn = document.getElementById ('scrollup');
 window.addEventListener('scroll', function(){
@@ -94,10 +41,6 @@ window.addEventListener('scroll', function(){
     }
 })
 
-let wHeight = window.innerHeight;
-window.addEventListener('resize', function(){
-    wHeight = window.innerHeight;
-})
 
 let sectionHight = 0
 for (let link of document.getElementsByClassName('nav_link')){
@@ -130,3 +73,23 @@ window.addEventListener('scroll', function(e) {
     }
 
 })
+///////////burger///////
+
+$(document).ready(function(){
+  $('.header__burger').click(function(event){
+    $('.header__burger, .nav').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+////////////////////////animation///
+var button = document.querySelector(".form__btn");
+
+button.onmousemove = (e) => {
+   var x = e.pageX - e.target.offsetLeft;
+   var y = e.pageY - e.target.offsetTop;
+
+   button.style.setProperty("--x", x + "px");
+   button.style.setProperty("--y", y + "px");
+};
+
