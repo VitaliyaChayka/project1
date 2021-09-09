0794f9ff0b572ed70ea1cd69701b8003c67ac5a2
Vitaliya_Chayka <vialife79@gmail.com>
2021-09-09 12:13:55 +0300

    js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,55 +24,6 @@ $('.slider').slick({
 
 
 
-////////////////////////////////// icon scrollup
-
-let upBtn = document.getElementById ('scrollup');
-window.addEventListener('scroll', function(){
-    if (wHeight < window.scrollY){
-        upBtn.classList.add('scrollup_active')
-    }
-    else {
-        upBtn.classList.remove('scrollup_active')
-
-    }
-})
-
-let wHeight = window.innerHeight;
-window.addEventListener('resize', function(){
-    wHeight = window.innerHeight;
-})
-
-let sectionHight = 0
-for (let link of document.getElementsByClassName('nav_link')){
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        let id_section = this.getAttribute('href') //#section
-        let pos = document.querySelector(id_section).getBoundingClientRect();
-        window.scrollTo({
-        top: document.querySelector(id_section).offsetTop - 110,
-        left: 0,
-        behavior: 'smooth'
-    });
-    })
-}
-
-
-
-window.addEventListener('scroll', function(e) {
-    let blocks = document.querySelectorAll('.block')
-    for (let block of blocks){
-    let pos = block.getBoundingClientRect()
-    let posTop = pos.top
-    let posHeight = pos.height
-    if (posTop < wHeight - posHeight && posTop > - posHeight){
-        block.classList.add('anim')
-    }
-   else {
-    block.classList.remove('anim')
-}
-    }
-
-})
 ///////////burger///////
 
 $(document).ready(function(){
